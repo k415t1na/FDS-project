1f32f5e5edf4e8f76cc7e1744492850223790fe8
Kristina Myftaraga <knm181@aubg.edu>
2020-03-23 16:40:36 +0200

    Initial Commit

---
+++ ProjectFDS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectFDS
+//
+//  Created by Kristina Myftaraga on 3/23/20.
+//  Copyright © 2020 Kristina Myftaraga. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
